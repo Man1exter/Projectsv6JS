d03340711220020fd3f46495cd76430834e1e4de
Mariusz Perzynski <m.perzyk@interia.pl>
2020-09-30 19:05:36 +0200

    download eleemnets to string in js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let score;
+const button1 = document.querySelector(".plus");
+const button2 = document.querySelector(".minus");
+const p1 = document.querySelector("p");
 
 const sums = (first,second) => {
  first + second == score;
